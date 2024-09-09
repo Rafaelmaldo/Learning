ec0329d5a558d3fe6532a7dc723939d365f751c3
Rafael Maldonado <rafaelmaldo387@gmail.com>
2024-09-08 20:37:18 -0400

    sorry didnt know I couldnt use the reduce and flat function shouldve listened longer

--- recursion/nested-arrays.js
+++ recursion/nested-arrays.js
@@ -1,14 +1,20 @@
+
+// * Rules
+//  - No using flatten or reduce
+
+
 function myNestedAdd(array) {
+  let sum = 0;
   for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i]) ) {
-      return nestedAdd(array.flat());
+    let currentValue = array[i];
+    if (Array.isArray(currentValue) ) {
+      sum += nestedAdd(currentValue);
+    } else {
+      sum += currentValue;
     }
   }
 
-  // BASE CASE
-  return array.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  })
+  return sum;
 }
 
 function nestedAdd(array) {
@@ -24,4 +30,4 @@ function nestedAdd(array) {
   return sum;
 }
 
-console.log(nestedAdd([1,2,3,[4,5,6,7,8]]))
+console.log(myNestedAdd([1,2,3,[4,5,6,7,8]]))
